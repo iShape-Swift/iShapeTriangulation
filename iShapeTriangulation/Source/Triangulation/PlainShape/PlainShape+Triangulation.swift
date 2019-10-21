acfecc53e2b6288ceef2d0385e278771072bd570
Nail Sharipov <nailxsharipov@gmail.com>
2019-10-21 09:57:56 +0300

    fix plain triangulation

--- iShapeTriangulation/Source/Triangulation/PlainShape/PlainShape+Triangulation.swift
+++ iShapeTriangulation/Source/Triangulation/PlainShape/PlainShape+Triangulation.swift
@@ -19,135 +19,137 @@ public extension PlainShape {
         triangles.reserveCapacity(3 * totalCount)
         var links = layout.links
         for index in layout.indices {
-            PlainShape.triangulate(index: index, links: inout links, triangles: &triangles)
+            PlainShape.triangulate(index: index, links: &links, triangles: &triangles)
         }
         
         return triangles
     }
-/*
-    private static func triangulate(index: Int, links: [Link], triangles: inout Array<Int>) {
+    
+    private static func triangulate(index: Int, links: inout [Link], triangles: inout Array<Int>) {
         var c = links[index]
         
         var a0 = links[c.next]
         var b0 = links[c.prev]
         
-        repeat {
-            let aBit0 = a0.vertex.point.bitPack
-            let bBit0 = b0.vertex.point.bitPack
-            
-            if bBit0 < aBit0 {
-                let b1 = links[b0.prev]
-                let bBit1 = b1.vertex.point.bitPack
-                if bBit1 < aBit0 {
-                    let orientation = PlainShape.getOrientation(a: c.vertex.point, b: b1.vertex.point, c: b0.vertex.point)
-                    
-                    switch orientation {
-                    case .clockWise:
-                        triangles.append(c.vertex.index)
-                        triangles.append(b1.vertex.index)
-                        triangles.append(b0.vertex.index)
-                        fallthrough
-                    case .line:     // don't add this triangle
-                        b0 = b1
-                        continue
-                    default:
-                        break
-                    }
-                }
-            } else {
+        next_point:
+            while a0.this != b0.this {
                 let a1 = links[a0.next]
-                let aBit1 = a1.vertex.point.bitPack
-                if aBit1 < bBit0 {
-                    let orientation = PlainShape.getOrientation(a: c.vertex.point, b: a0.vertex.point, c: a1.vertex.point)
-                    
-                    switch orientation {
-                    case .clockWise:
+                let b1 = links[b0.prev]
+                
+                let aBit0 = a0.vertex.point.bitPack
+                var aBit1 = a1.vertex.point.bitPack
+                if aBit1 < aBit0 {
+                    aBit1 = aBit0
+                }
+                
+                let bBit0 = b0.vertex.point.bitPack
+                var bBit1 = b1.vertex.point.bitPack
+                if bBit1 < bBit0 {
+                    bBit1 = bBit0
+                }
+                
+                if aBit0 < bBit1 && bBit0 < aBit1 {
+                    if PlainShape.isNotLine(a: c.vertex.point, b: a0.vertex.point, c: b0.vertex.point) {
                         triangles.append(c.vertex.index)
                         triangles.append(a0.vertex.index)
-                        triangles.append(a1.vertex.index)
-                        fallthrough
-                    case .line:
+                        triangles.append(b0.vertex.index)
+                    }
+                    
+                    a0.prev = b0.this
+                    b0.next = a0.this
+                    links[a0.this] = a0
+                    links[b0.this] = b0
+                    
+                    if bBit0 < aBit0 {
+                        c = b0
+                        b0 = b1
+                    } else {
+                        c = a0
                         a0 = a1
-                        continue
-                    default:
-                        break
+                    }
+                } else {
+                    if aBit1 < bBit1 {
+                        var cx = c
+                        var ax0 = a0
+                        var ax1 = a1
+
+                        repeat {
+                            let orientation = PlainShape.getOrientation(a: cx.vertex.point, b: ax0.vertex.point, c: ax1.vertex.point)
+                            switch orientation {
+                            case .clockWise:
+                                triangles.append(cx.vertex.index)
+                                triangles.append(ax0.vertex.index)
+                                triangles.append(ax1.vertex.index)
+                                fallthrough
+                            case .line:
+                                ax1.prev = cx.this
+                                cx.next = ax1.this
+                                links[cx.this] = cx
+                                links[ax1.this] = ax1
+                                if cx.this != c.this {
+                                    ax0 = cx
+                                    cx = links[cx.prev]
+                                    break
+                                } else {
+                                    c = links[c.this]
+                                    a0 = links[c.next]
+                                    continue next_point
+                                }
+                            case .counterClockWise:
+                                cx = ax0
+                                ax0 = ax1
+                                ax1 = links[ax1.next]
+                            }
+                        } while ax1.vertex.point.bitPack < bBit1
+                    } else {
+                        var cx = c
+                        var bx0 = b0
+                        var bx1 = b1
+                        repeat {
+                            let orientation = PlainShape.getOrientation(a: cx.vertex.point, b: bx1.vertex.point, c: bx0.vertex.point)
+                            switch orientation {
+                            case .clockWise:
+                                triangles.append(cx.vertex.index)
+                                triangles.append(bx1.vertex.index)
+                                triangles.append(bx0.vertex.index)
+                                fallthrough
+                            case .line:
+                                bx1.next = cx.this
+                                cx.prev = bx1.this
+                                links[cx.this] = cx
+                                links[bx1.this] = bx1
+                                if cx.this != c.this {
+                                    bx0 = cx
+                                    cx = links[cx.next]
+                                    break
+                                } else {
+                                    c = links[c.this]
+                                    b0 = links[c.prev]
+                                    continue next_point
+                                }
+                            case .counterClockWise:
+                                cx = bx0
+                                bx0 = bx1
+                                bx1 = links[bx1.prev]
+                            }
+                        } while bx1.vertex.point.bitPack < aBit1
+                    }
+                    
+                    triangles.append(c.vertex.index)
+                    triangles.append(a0.vertex.index)
+                    triangles.append(b0.vertex.index)
+                    
+                    if bBit0 < aBit0 {
+                        c = b0
+                        b0 = b1
+                    } else {
+                        c = a0
+                        a0 = a1
                     }
                 }
-            }
-            
-            triangles.append(c.vertex.index)
-            triangles.append(a0.vertex.index)
-            triangles.append(b0.vertex.index)
-
-            if bBit0 < aBit0 {
-                c = b0
-                b0 = links[b0.prev]
-            } else {
-                c = a0
-                a0 = links[a0.next]
-            }
-        } while a0.this != b0.this
+        }
     }
-    */
     
-    private static func triangulate(index: Int, inout links: [Link], triangles: inout Array<Int>) {
-        var c = links[index]
-        
-        var a0 = links[c.next]
-        var b0 = links[c.prev]
-        
-        repeat {
-            let a1 = links[a0.next]
-            let b1 = links[b0.prev]
-            
-            let aBit0 = a0.vertex.point.bitPack;
-            let aBit1 = a1.vertex.point.bitPack;
-            let bBit0 = b0.vertex.point.bitPack;
-            let bBit1 = b1.vertex.point.bitPack;
-            
-            if aBit0 < bBit1 && bBit0 < aBit1 {
-                triangles.append(c.vertex.index)
-                triangles.append(a0.vertex.index)
-                triangles.append(b0.vertex.index)
-
-                if bBit0 < aBit0 {
-                    c = b0
-                    b0 = b1
-                } else {
-                    c = a0
-                    a0 = a1
-                }
-            } else {
-                if aBit1 < bBit0 {
-                    var cx = c
-                    var ax0 = a0
-                    var ax1 = a1
-                    repeat {
-                        let orientation = PlainShape.getOrientation(a: cx.vertex.point, b: ax0.vertex.point, c: ax1.vertex.point)
-                        switch orientation {
-                        case .clockWise:
-                            triangles.append(cx.vertex.index)
-                            triangles.append(a0.vertex.index)
-                            triangles.append(a1.vertex.index)
-                        case .line:
-                            ax1.prev = cx.this
-                            cx.next = ax1.this
-                            links[cx.this] = cx
-                            links[ax1.this] = ax1
-                        default:
-                            break
-                        }
-                        
-                        ax0 = ax1
-                        ax1 = links[ax0.next]
-                    } while ax1.vertex.point.bitPack < bBit1
-                }
-            }
-
-            
-        } while a0.this != b0.this
-    }
-
     private enum Orientation {
         case clockWise
         case counterClockWise
@@ -165,5 +167,11 @@ public extension PlainShape {
         } else {
             return .line
         }
+    }
+    
+    private static func isNotLine(a: IntPoint, b: IntPoint, c: IntPoint) -> Bool {
+        let m0 = (c.y - a.y) * (b.x - a.x)
+        let m1 = (b.y - a.y) * (c.x - a.x)
+        return m0 != m1
     }
 }
