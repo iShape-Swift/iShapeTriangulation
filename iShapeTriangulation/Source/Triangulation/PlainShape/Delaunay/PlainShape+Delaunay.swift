fddb740597bc3c070017815928a93d0aff8cc63f
Nail Sharipov <nailxsharipov@gmail.com>
2019-10-22 09:53:14 +0300

    new Delaunay implementation

--- iShapeTriangulation/Source/Triangulation/PlainShape/Delaunay/PlainShape+Delaunay.swift
+++ iShapeTriangulation/Source/Triangulation/PlainShape/Delaunay/PlainShape+Delaunay.swift
@@ -12,69 +12,93 @@ import iGeometry
 public extension PlainShape {
     
     private struct Edge {
-        static let empty = Edge(a: -1, b: -1, neighbor: -1, number: -1)
-        let a: Index            // vertex index
-        let b: Index            // vertex index
-        let neighbor: Index     // prev triangle index
-        let number: Index       // edge index 0 - a(bc), 1 - b(ac), 2 - c(ab)
-        
-        var isEmpty: Bool {
-            return neighbor == -1
-        }
-        
-        var isNotEmpty: Bool {
-            return neighbor >= 0
-        }
+        let a: Int            // vertex index
+        let b: Int            // vertex index
+        let neighbor: Int     // prev triangle index
     }
     
-    private struct EdgeBuffer {
-        private var e0 = Edge.empty
-        private var e1 = Edge.empty
+    private struct TriangleStack {
         
-        mutating func push(slice: Edge) {
-            #if iShapeTest
-            if !(e0.isEmpty || e1.isEmpty) {
-                assertionFailure("cannot be full")
-            }
-            #endif
-            if e0.isEmpty {
-                e0 = slice
-            } else {
-                e1 = slice
-            }
+        private var edges: [Edge]
+        private (set) var triangles: [Delaunay.Triangle]
+        private var counter: Int
+        
+        init(count: Int) {
+            self.counter = 0
+            self.edges = Array<Edge>()
+            self.edges.reserveCapacity(8)
+            self.triangles = Array<Delaunay.Triangle>(repeating: .init(), count: count)
+        }
+
+        mutating func reset() {
+            edges.removeAll(keepingCapacity: true)
         }
         
-        mutating func pop(a: Index, b: Index) -> Edge {
-            let isFirst = (e0.a == a || e0.a == b) && (e0.b == a || e0.b == b)
-            let e: Edge
-            if isFirst {
-                e = e0
-                e0 = .empty
-            } else {
-                e = e1
-                e1 = .empty
+        mutating func add(a: Vertex, b: Vertex, c: Vertex) {
+            var triangle = Delaunay.Triangle(
+                 index: counter,
+                 a: a,
+                 b: b,
+                 c: c
+             )
+
+            self.edges.append(Edge(a: b.index, b: c.index, neighbor: triangle.index)) // bc is always slice
+
+            if let ac = self.pop(a: a.index, b: c.index) {
+                var neighbor = triangles[ac.neighbor]
+                neighbor.neighbors[0] = triangle.index
+                triangle.neighbors[1] = neighbor.index
+                triangles[neighbor.index] = neighbor
             }
             
-            return e
+            if let ab = self.pop(a: a.index, b: b.index) {
+                var neighbor = triangles[ab.neighbor]
+                neighbor.neighbors[0] = triangle.index
+                triangle.neighbors[2] = neighbor.index
+                triangles[neighbor.index] = neighbor
+            }
+
+            triangles[triangle.index] = triangle
+
+            counter += 1
+        }
+        
+        private mutating func pop(a: Index, b: Index) -> Edge? {
+            let last = edges.count - 1
+            var i = 0
+            repeat {
+                let e = edges[i]
+                if (e.a == a || e.a == b) && (e.b == a || e.b == b) {
+                    if i != last {
+                        edges[i] = edges[last]
+                    }
+                    edges.removeLast()
+                    return e
+                }
+                i += 1
+            } while i <= last
+            return nil
         }
     }
-    
+
     func triangulateDelaunay() -> [Int] {
         let layout = self.split()
         
         let vertexCount = self.points.count
         
         let totalCount = vertexCount &+ ((self.layouts.count - 2) << 1)
+
+        var triangleStack = TriangleStack(count: totalCount)
         
-        var triangles = Array<Delaunay.Triangle>(repeating: .init(), count: totalCount)
-        var counter = 0
         var links = layout.links
-        var slices = layout.slices
         for index in layout.indices {
-            PlainShape.triangulate(index: index, counter: &counter, links: &links, triangles: &triangles, slices: &slices)
+            triangleStack.reset()
+            PlainShape.triangulate(index: index, links: &links, triangleStack: &triangleStack)
         }
         
-        var sliceBuffer = SliceBuffer(vertexCount: vertexCount, slices: slices)
+        var triangles = triangleStack.triangles
+        
+        var sliceBuffer = SliceBuffer(vertexCount: vertexCount, slices: layout.slices)
         sliceBuffer.addConections(triangles: &triangles)
         
         var delaunay = Delaunay(triangles: triangles)
@@ -83,204 +107,13 @@ public extension PlainShape {
         return delaunay.indices
     }
     
-    /*
-     private func triangulate(index: Int, counter: inout Int, links: [Link], triangles: inout [Delaunay.Triangle]) {
-     
-     var c = links[index]
-     
-     var a0 = links[c.next]
-     var b0 = links[c.prev]
-     
-     var edgeBuffer = EdgeBuffer()
-     
-     repeat {
-     let aBit0 = a0.vertex.point.bitPack
-     let bBit0 = b0.vertex.point.bitPack
-     
-     if bBit0 < aBit0 {
-     let b1 = links[b0.prev]
-     let bBit1 = b1.vertex.point.bitPack
-     if bBit1 < aBit0 && PlainShape.isCCW_or_Line(a: c.vertex.point, b: b1.vertex.point, c: b0.vertex.point) {
-     if c.next != b0.this {
-     // in middle
-     
-     let slice = edgeBuffer.pop(a: c.vertex.index, b: b0.vertex.index)
-     
-     triangles[counter] = Delaunay.Triangle(
-     index: counter,
-     a: b1.vertex,
-     b: b0.vertex,
-     c: c.vertex,
-     bc: slice.neighbor
-     )
-     if slice.isNotEmpty {
-     var neighbor = triangles[slice.neighbor]
-     neighbor.neighbors[slice.number] = counter
-     triangles[slice.neighbor] = neighbor
-     }
-     edgeBuffer.push(slice: Edge(a: c.vertex.index, b: b1.vertex.index, neighbor: counter, number: 1))
-     } else {
-     // on border
-     
-     triangles[counter] = Delaunay.Triangle(
-     index: counter,
-     a: b0.vertex,
-     b: c.vertex,
-     c: b1.vertex
-     )
-     
-     edgeBuffer.push(slice: Edge(a: c.vertex.index, b: b1.vertex.index, neighbor: counter, number: 0))
-     }
-     
-     counter &+= 1
-     
-     b0 = b1
-     
-     continue
-     }
-     } else {
-     let a1 = links[a0.next]
-     let aBit1 = a1.vertex.point.bitPack
-     if aBit1 < bBit0 && PlainShape.isCCW_or_Line(a: c.vertex.point, b: a0.vertex.point, c: a1.vertex.point) {
-     if c.next != a0.this {
-     // in middle
-     
-     let slice = edgeBuffer.pop(a: c.vertex.index, b: a0.vertex.index)
-     
-     triangles[counter] = Delaunay.Triangle(
-     index: counter,
-     a: a1.vertex,
-     b: c.vertex,
-     c: a0.vertex,
-     bc: slice.neighbor
-     )
-     if slice.isNotEmpty {
-     var neighbor = triangles[slice.neighbor]
-     neighbor.neighbors[slice.number] = counter
-     triangles[slice.neighbor] = neighbor
-     }
-     
-     edgeBuffer.push(slice: Edge(a: c.vertex.index, b: a1.vertex.index, neighbor: counter, number: 2))
-     } else {
-     // on border
-     
-     triangles[counter] = Delaunay.Triangle(
-     index: counter,
-     a: a0.vertex,
-     b: a1.vertex,
-     c: c.vertex
-     )
-     
-     edgeBuffer.push(slice: Edge(a: c.vertex.index, b: a1.vertex.index, neighbor: counter, number: 0))
-     }
-     
-     counter &+= 1
-     
-     a0 = a1
-     
-     continue
-     }
-     }
-     
-     if c.next == a0.this || c.prev == b0.this {
-     if c.next == a0.this {
-     // c - a0 edge on board
-     
-     let slice = edgeBuffer.pop(a: c.vertex.index, b: b0.vertex.index)
-     
-     triangles[counter] = Delaunay.Triangle(
-     index: counter,
-     a: a0.vertex,
-     b: b0.vertex,
-     c: c.vertex,
-     bc: slice.neighbor
-     )
-     if slice.isNotEmpty {
-     var neighbor = triangles[slice.neighbor]
-     neighbor.neighbors[slice.number] = counter
-     triangles[slice.neighbor] = neighbor
-     }
-     
-     edgeBuffer.push(slice: Edge(a: b0.vertex.index, b: a0.vertex.index, neighbor: counter, number: 2))
-     } else {
-     // c - b0 edge on board
-     
-     let slice = edgeBuffer.pop(a: c.vertex.index, b: a0.vertex.index)
-     
-     triangles[counter] = Delaunay.Triangle(
-     index: counter,
-     a: b0.vertex,
-     b: c.vertex,
-     c: a0.vertex,
-     bc: slice.neighbor
-     )
-     if slice.isNotEmpty {
-     var neighbor = triangles[slice.neighbor]
-     neighbor.neighbors[slice.number] = counter
-     triangles[slice.neighbor] = neighbor
-     }
-     
-     edgeBuffer.push(slice: Edge(a: a0.vertex.index, b: b0.vertex.index, neighbor: counter, number: 1))
-     }
-     } else {
-     // all triangle's edges inside
-     
-     let sliceA = edgeBuffer.pop(a: c.vertex.index, b: b0.vertex.index)
-     let sliceB = edgeBuffer.pop(a: c.vertex.index, b: a0.vertex.index)
-     
-     triangles[counter] = Delaunay.Triangle(
-     index: counter,
-     a: c.vertex,
-     b: a0.vertex,
-     c: b0.vertex,
-     ac: sliceA.neighbor,
-     ab: sliceB.neighbor
-     )
-     
-     if sliceA.isNotEmpty {
-     var neighbor = triangles[sliceA.neighbor]
-     neighbor.neighbors[sliceA.number] = counter
-     triangles[sliceA.neighbor] = neighbor
-     }
-     
-     if sliceB.isNotEmpty {
-     var neighbor = triangles[sliceB.neighbor]
-     neighbor.neighbors[sliceB.number] = counter
-     triangles[sliceB.neighbor] = neighbor
-     }
-     
-     edgeBuffer.push(slice: Edge(a: a0.vertex.index, b: b0.vertex.index, neighbor: counter, number: 0))
-     }
-     
-     if bBit0 < aBit0 {
-     c = b0
-     b0 = links[b0.prev]
-     } else {
-     c = a0
-     a0 = links[a0.next]
-     }
-     
-     counter &+= 1
-     
-     } while a0.this != b0.this
-     
-     var neighbors = triangles[counter &- 1].neighbors
-     for i in 0...2 {
-     let neighbor = neighbors[i]
-     neighbors[i] = neighbor < counter ? neighbor : -1
-     }
-     triangles[counter &- 1].neighbors = neighbors
-     }
-     */
-    
-    
-    private static func triangulate(index: Int, counter: inout Int, links: inout [Link], triangles: inout [Delaunay.Triangle], slices: inout [Slice]) {
+    private static func triangulate(index: Int, links: inout [Link], triangleStack: inout TriangleStack) {
         
         var c = links[index]
         
         var a0 = links[c.next]
         var b0 = links[c.prev]
-        
+
         next_point:
             while a0.this != b0.this {
                 let a1 = links[a0.next]
@@ -299,16 +132,9 @@ public extension PlainShape {
                 }
                 
                 if aBit0 < bBit1 && bBit0 < aBit1 {
-                    triangles[counter] = Delaunay.Triangle(
-                        index: counter,
-                        a: c.vertex,
-                        b: a0.vertex,
-                        c: b0.vertex
-                    )
-                    counter += 1
-                    
-                    slices.append(Slice(a: a0.vertex.index, b: b0.vertex.index))
-                    
+
+                    triangleStack.add(a: c.vertex, b: a0.vertex, c: b0.vertex)
+
                     a0.prev = b0.this
                     b0.next = a0.this
                     links[a0.this] = a0
@@ -316,10 +142,10 @@ public extension PlainShape {
                     
                     if bBit0 < aBit0 {
                         c = b0
-                        b0 = b1
+                        b0 = links[b0.prev]
                     } else {
                         c = a0
-                        a0 = a1
+                        a0 = links[a0.next]
                     }
                 } else {
                     if aBit1 < bBit1 {
@@ -330,15 +156,7 @@ public extension PlainShape {
                         repeat {
                             let isCCW = PlainShape.isCCW_or_Line(a: cx.vertex.point, b: ax0.vertex.point, c: ax1.vertex.point)
                             if isCCW {
-                                triangles[counter] = Delaunay.Triangle(
-                                    index: counter,
-                                    a: cx.vertex,
-                                    b: ax0.vertex,
-                                    c: ax1.vertex
-                                )
-                                counter += 1
-                                
-                                slices.append(Slice(a: cx.vertex.index, b: ax1.vertex.index))
+                                triangleStack.add(a: cx.vertex, b: ax0.vertex, c: ax1.vertex)
                                 
                                 ax1.prev = cx.this
                                 cx.next = ax1.this
@@ -366,15 +184,7 @@ public extension PlainShape {
                         repeat {
                             let isCCW = PlainShape.isCCW_or_Line(a: cx.vertex.point, b: bx1.vertex.point, c: bx0.vertex.point)
                             if isCCW {
-                                triangles[counter] = Delaunay.Triangle(
-                                    index: counter,
-                                    a: cx.vertex,
-                                    b: bx1.vertex,
-                                    c: bx0.vertex
-                                )
-                                counter += 1
-                                
-                                slices.append(Slice(a: cx.vertex.index, b: bx1.vertex.index))
+                                triangleStack.add(a: bx0.vertex, b: cx.vertex, c: bx1.vertex)
                                 
                                 bx1.next = cx.this
                                 cx.prev = bx1.this
@@ -396,20 +206,12 @@ public extension PlainShape {
                             }
                         } while bx1.vertex.point.bitPack <= aBit1
                     }
-                    
-                    triangles[counter] = Delaunay.Triangle(
-                        index: counter,
-                        a: c.vertex,
-                        b: a0.vertex,
-                        c: b0.vertex
-                    )
-                    counter += 1
-                    
-                    slices.append(Slice(a: a0.vertex.index, b: b0.vertex.index))
-                    
+
                     a0 = links[a0.this]
                     b0 = links[b0.this]
                     
+                    triangleStack.add(a: c.vertex, b: a0.vertex, c: b0.vertex)
+                   
                     a0.prev = b0.this
                     b0.next = a0.this
                     links[a0.this] = a0
