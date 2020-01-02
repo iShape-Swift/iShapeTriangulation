4c9984c3cf8ec5b9fe902e1b89ec766fcb4138a2
Nail Sharipov <nailxsharipov@gmail.com>
2020-01-03 00:23:59 +0500

    add test

--- iShapeTests/Test/iGeometryTests.swift
+++ iShapeTests/Test/iGeometryTests.swift
@@ -47,6 +47,41 @@ final class iGeometryTests: XCTestCase {
         XCTAssertEqual(points, [a, b])
     }
     
+    func testShapeList_00() {
+        let a = PlainShape(iShape: IntShape(
+            hull: [IntPoint(x: 10, y: 20), IntPoint(x: 30, y: 40), IntPoint(x: 50, y: 60)],
+            holes: [
+                [IntPoint(x: 0, y: 1), IntPoint(x: 2, y: 3), IntPoint(x: 4, y: 5), IntPoint(x: 6, y: 7)],
+                [IntPoint(x: 8, y: 9), IntPoint(x: 11, y: 12), IntPoint(x: 13, y: 14)]
+            ]
+        ))
+        let b = PlainShape(iShape: IntShape(
+            hull: [IntPoint(x: 70, y: 80), IntPoint(x: 90, y: 100), IntPoint(x: 110, y: 120)],
+            holes: [
+                [IntPoint(x: 15, y: 16), IntPoint(x: 17, y: 18), IntPoint(x: 19, y: 21), IntPoint(x: 22, y: 23), IntPoint(x: 24, y: 25)],
+                [IntPoint(x: 26, y: 27), IntPoint(x: 28, y: 29), IntPoint(x: 31, y: 32), IntPoint(x: 33, y: 34)],
+                [IntPoint(x: 35, y: 36), IntPoint(x: 37, y: 38), IntPoint(x: 39, y: 41), IntPoint(x: 42, y: 43)]
+            ]
+        ))
+        let c = PlainShape(iShape: IntShape(
+            hull: [IntPoint(x: 130, y: 140), IntPoint(x: 150, y: 160), IntPoint(x: 170, y: 180)],
+            holes: []
+        ))
+        
+        var shapeList = PlainShapeList(minimumPointsCapacity: 10, minimumLayoutsCapacity: 4, minimumSegmentsCapacity: 2)
+        shapeList.add(plainShape: a)
+        shapeList.add(plainShape: b)
+        shapeList.add(plainShape: c)
+        
+        let a0 = shapeList.get(index: 0)
+        let b0 = shapeList.get(index: 1)
+        let c0 = shapeList.get(index: 2)
+        
+        XCTAssertEqual(a0, a)
+        XCTAssertEqual(b0, b)
+        XCTAssertEqual(c0, c)
+    }
+    
     func testValidation_00() {
         let shape = Shape(hull: [Point(x: -1, y: 0), Point(x: 0, y: 1), Point(x: 1, y: 0)], holes: [])
         let iShape = IntShape(shape: shape)
