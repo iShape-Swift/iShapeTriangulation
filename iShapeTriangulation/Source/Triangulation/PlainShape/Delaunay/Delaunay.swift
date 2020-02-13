58ff888c35e6613f512cda73b1d722d1d7a56c47
Nail Sharipov <nailxsharipov@gmail.com>
2020-02-13 23:22:20 +0300

    move api

--- iShapeTriangulation/Source/Triangulation/PlainShape/Delaunay/Delaunay.swift
+++ iShapeTriangulation/Source/Triangulation/PlainShape/Delaunay/Delaunay.swift
@@ -322,51 +322,10 @@ struct Delaunay {
             return result > -1000000000
         }
     }
-}
-
-#if iShapeTest
-
-extension Delaunay {
     
-    struct Circle {
-        let center: Point
-        let radius: Float
-    }
-
-    public static func circumscribedСircleCenter(a: Point, b: Point, c: Point) -> Circle {
-        let d = 2 * (a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y))
-        let x = ((a.x * a.x + a.y * a.y) * (b.y - c.y) + (b.x * b.x + b.y * b.y) * (c.y - a.y) + (c.x * c.x + c.y * c.y) * (a.y - b.y)) / d
-        let y = ((a.x * a.x + a.y * a.y) * (c.x - b.x) + (b.x * b.x + b.y * b.y) * (a.x - c.x) + (c.x * c.x + c.y * c.y) * (b.x - a.x)) / d
-        
-        let r = sqrt((a.x - x) * (a.x - x) + (a.y - y) * (a.y - y))
-        
-        return Circle(center: Point(x: x, y: y), radius: r)
-    }
-    
-    public static func inscribedСircle(a: Point, b: Point, c: Point) -> Circle {
-        let ABx = a.x - b.x
-        let ABy = a.y - b.y
-        let AB = sqrt(ABx * ABx + ABy * ABy)
-        
-        let ACx = a.x - c.x
-        let ACy = a.y - c.y
-        let AC = sqrt(ACx * ACx + ACy * ACy)
-        
-        let BCx = b.x - c.x
-        let BCy = b.y - c.y
-        let BC = sqrt(BCx * BCx + BCy * BCy)
-        
-        let p = AB + BC + AC
-        
-        let Ox = (BC * a.x + AC * b.x + AB * c.x) / p
-        let Oy = (BC * a.y + AC * b.y + AB * c.y) / p
-        
-        let r = sqrt((-BC + AC + AB) * (BC - AC + AB) * (BC + AC - AB) / (4 * p))
-        return Circle(center: Point(x: Ox, y: Oy), radius: r)
-    }
-    
-    public static func verefy(p: IntPoint, a: IntPoint, b: IntPoint, c: IntPoint) -> Bool {
+    #if iShapeTest
+    static func verefy(p: IntPoint, a: IntPoint, b: IntPoint, c: IntPoint) -> Bool {
         return Delaunay.isPrefect(p: p, a: a, b: b, c: c)
     }
+    #endif
 }
-#endif
