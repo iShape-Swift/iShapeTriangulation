fdb5711b644e40de3a94cbb8ed8105477a3ff095
Nail Sharipov <nailxsharipov@gmail.com>
2020-05-07 12:42:07 +0500

    add more useful public api

--- iShapeTriangulation/Source/Triangulation/Triangulator.swift
+++ iShapeTriangulation/Source/Triangulation/Triangulator.swift
@@ -32,24 +32,7 @@ public struct Triangulator {
     /// - Parameter hull: range of the hull vertices in points array
     /// - Parameter holes: array of ranges for all holes
     public func triangulate(points: [CGPoint], hull: ArraySlice<CGPoint>?, holes: [ArraySlice<CGPoint>]?) -> [Int] {
-        
-        let intPoints = iGeom.int(points: points.toPoints())
-
-        var layouts = [PlainShape.Layout]()
-
-        if let hull = hull, let holes = holes {
-            layouts.reserveCapacity(holes.count + 1)
-            layouts.append(PlainShape.Layout(begin: 0, length: hull.endIndex, isClockWise: true))
-            for hole in holes {
-                let length = hole.endIndex - hole.startIndex
-                layouts.append(PlainShape.Layout(begin: hole.startIndex, length: length, isClockWise: false))
-            }
-        } else {
-            layouts.append(PlainShape.Layout(begin: 0, length: intPoints.count, isClockWise: true))
-        }
-
-        let shape = PlainShape(points: intPoints, layouts: layouts)
-        
+        let shape = PlainShape(iGeom: iGeom, points: points, hull: hull, holes: holes)
         return shape.triangulate()
     }
     
@@ -58,23 +41,7 @@ public struct Triangulator {
     /// - Parameter hull: range of the hull vertices in points array
     /// - Parameter holes: array of ranges for all holes
     public func triangulateDelaunay(points: [CGPoint], hull: ArraySlice<CGPoint>?, holes: [ArraySlice<CGPoint>]?) -> [Int] {
-        let intPoints = iGeom.int(points: points.toPoints())
-
-        var layouts = [PlainShape.Layout]()
-
-        if let hull = hull, let holes = holes {
-            layouts.reserveCapacity(holes.count + 1)
-            layouts.append(PlainShape.Layout(begin: 0, length: hull.endIndex, isClockWise: true))
-            for hole in holes {
-                let length = hole.endIndex - hole.startIndex
-                layouts.append(PlainShape.Layout(begin: hole.startIndex, length: length, isClockWise: false))
-            }
-        } else {
-            layouts.append(PlainShape.Layout(begin: 0, length: intPoints.count, isClockWise: true))
-        }
-
-        let shape = PlainShape(points: intPoints, layouts: layouts)
-        
+        let shape = PlainShape(iGeom: iGeom, points: points, hull: hull, holes: holes)
         return shape.delaunay().trianglesIndices
     }
     
@@ -84,6 +51,31 @@ public struct Triangulator {
     ///   - hull: range of the hull vertices in points array
     ///   - holes: array of ranges for all holes
     public func polygonate(points: [CGPoint], hull: ArraySlice<CGPoint>?, holes: [ArraySlice<CGPoint>]?) -> [Int] {
+        let shape = PlainShape(iGeom: iGeom, points: points, hull: hull, holes: holes)
+        return shape.delaunay().convexPolygonsIndices
+    }
+
+}
+
+public extension PlainShape {
+
+    /// Data to represent complex shape
+    /// - Parameters:
+    ///   - precision: The minimum required precision. It's a minimum linear distance after which points will be recognized as the same.
+    ///   - points: Linear array of all your polygon vertices. All hull's vertices must be list in clockwise order. All holes vertices must be list in counterclockwise order.
+    ///   - hull: range of the hull vertices in points array
+    ///   - holes: array of ranges for all holes
+    init(precision: CGFloat = 0.0001, points: [CGPoint], hull: ArraySlice<CGPoint>?, holes: [ArraySlice<CGPoint>]?) {
+        self.init(iGeom: IntGeom(scale: Float(1 / precision)), points: points, hull: hull, holes: holes)
+    }
+    
+    /// Data to represent complex shape
+    /// - Parameters:
+    ///   - iGeom: Int <-> Float converter
+    ///   - points: Linear array of all your polygon vertices. All hull's vertices must be list in clockwise order. All holes vertices must be list in counterclockwise order.
+    ///   - hull: range of the hull vertices in points array
+    ///   - holes: array of ranges for all holes
+    init(iGeom: IntGeom, points: [CGPoint], hull: ArraySlice<CGPoint>?, holes: [ArraySlice<CGPoint>]?) {
         let intPoints = iGeom.int(points: points.toPoints())
 
         var layouts = [PlainShape.Layout]()
@@ -98,10 +90,7 @@ public struct Triangulator {
         } else {
             layouts.append(PlainShape.Layout(begin: 0, length: intPoints.count, isClockWise: true))
         }
-
-        let shape = PlainShape(points: intPoints, layouts: layouts)
-        
-        return shape.delaunay().convexPolygonsIndices
+        self.init(points: intPoints, layouts: layouts)
     }
 
 }
