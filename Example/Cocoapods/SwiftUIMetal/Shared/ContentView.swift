efcf1cfed3d19dcd9c0e9889486024cbe40e3ec4
Nail Sharipov <nailxsharipov@gmail.com>
2022-01-05 23:30:49 +0500

    update examples

---
+++ Example/Cocoapods/SwiftUIMetal/Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Nail Sharipov on 05.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MetalView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/SwiftPackageManager/SwiftUIMetal/Shared/SwiftUIMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMetalApp.swift
+//  Shared
+//
+//  Created by Nail Sharipov on 05.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
